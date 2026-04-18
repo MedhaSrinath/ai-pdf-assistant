d1ef017767f1b7e28e42b5fef52a5e402915ccc9
Medha <medhabengaluru@gmail.com>
2026-04-18 13:58:48 +0530

    level3: embeddings done!

--- pdf_read.py
+++ pdf_read.py
@@ -1,32 +1,44 @@
 import requests
 from pypdf import PdfReader
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity 
+
+#load embedding model
+embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
 reader = PdfReader("Sample.pdf")
 
 text = ""
-
 for page in reader.pages:
     extracted = page.extract_text()
     if extracted:
         text += extracted
+
 #split into chunks
-def chunk_text(text,chunk_size=500):
-    chunks=[]
-    for i in range(0,len(text),chunk_size):
-        chunks.append(text[i:i+chunk_size])
-    return chunks
+def chunk_text(text, chunk_size=500):
+    return [text[i:i+chunk_size] for i in range(0,len(text), chunk_size)]
 
 chunks = chunk_text(text)
 
+#create embeddings for each chunk
+chunk_embeddings = embedder.encode(chunks)
+
 question = input("Ask something about the PDF: ")
+
+question_embedding = embedder.encode([question])
+
+#step6: find best chunk using similarity
+similarities = cosine_similarity(question_embedding, chunk_embeddings)[0]
+best_index = similarities.argmax()
+best_chunk = chunks[best_index]
 
 prompt = f"""
 You are a helpful assistant.
 
-Answer clearly and in simple terms using ONLY the context elow.
+Answer ONLU using the context below.
 
 Context:
-{text[:2000]}
+{best_chunk}
 
 Question:
 {question}
@@ -43,9 +55,6 @@ response = requests.post(
 
 data = response.json()
 
-if "response" in data:
-    print("\nAI says:\n")
-    print(data["response"])
-else:
-    print("Something went wrong:", data.get("error"))
-
+# Step 9: Output
+print("\nAI says:\n")
+print(data["response"])
