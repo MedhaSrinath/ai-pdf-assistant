63b41e967004c899604b82f90f84255d903daae7
Medha <medhasrinathbsc24@rvu.edu.in>
2026-04-18 11:39:22 +0530

    Initial commit - AI PDF Assistant

---
+++ local_ai.py
@@ -0,0 +1,19 @@
+import requests
+
+url= "http://localhost:11434/api/generate"
+
+user_input = input("Ask something: ")
+
+response=requests.post(
+    url,
+    json={
+        "model": "llama3",
+        "prompt": user_input,
+        "stream": False
+    }
+)
+
+data = response.json()
+
+print("\nAI says:\n")
+print(data["response"])
